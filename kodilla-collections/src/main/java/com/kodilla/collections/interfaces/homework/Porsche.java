3c5ab28375bc25a85d1d481ec606153d7264ef08
Mateusz Mekarski <matmeka6@gmail.com>
2020-08-19 15:12:43 +0200

    4.3

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/Porsche.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/Porsche.java
@@ -1,11 +1,19 @@
 package com.kodilla.collections.interfaces.homework;
 
+import java.util.Objects;
+
 public class Porsche implements Car {
     double speed;
 
     public Porsche(int speed) {
         this.speed=speed;
+
     }
+
+    public Porsche() {
+
+    }
+
 
     public double getSpeed() {
         return speed;
@@ -24,5 +32,18 @@ public class Porsche implements Car {
         return "Porsche{" +
                 "speed=" + speed +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Porsche porsche = (Porsche) o;
+        return Double.compare(porsche.speed, speed) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(speed);
     }
 }
--- kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
@@ -1,5 +1,6 @@
 package com.kodilla.collections.lists.homework;
 
+
 import com.kodilla.collections.interfaces.homework.Porsche;
 
 import java.util.ArrayList;
@@ -8,6 +9,8 @@ import java.util.List;
 public class CarsListApplication {
     public static void main(String[] args) {
         List<Porsche> cars = new ArrayList<>();
+        Porsche porsche1 = new Porsche(130);
+        cars.add(porsche1);
         cars.add(new Porsche(100));
         cars.add(new Porsche(80));
         cars.add(new Porsche(90));
@@ -15,7 +18,7 @@ public class CarsListApplication {
         cars.add(new Porsche(120));
 
         cars.remove(3);
-        //cars.remove();
+        cars.remove(porsche1);
         System.out.println(cars.size());
 
         for (Porsche porsche : cars){
