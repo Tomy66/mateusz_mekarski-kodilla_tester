6df37c08f8acdbc47a52c2ab3ecc88167509697f
Mateusz Mekarski <matmeka6@gmail.com>
2020-08-26 20:12:25 +0200

    2.3

--- kodilla-intro/src/main/java/com/kodilla/basics/FirstClass.java
+++ kodilla-intro/src/main/java/com/kodilla/basics/FirstClass.java
@@ -2,20 +2,20 @@ package com.kodilla.basics;
 
 public class FirstClass {
     public static void main(String[] args) {
-        Notebook notebook = new Notebook(600, 900, 2020);
+        Notebook notebook = new Notebook(600, 600, 2020);
         System.out.println(notebook.weight + " " + notebook.price + " " + notebook.year);
         notebook.checkPrice();
         notebook.checkWeight();
         notebook.checkYear();
 
         Notebook heavyNotebook = new Notebook(2000, 1500, 2015);
-        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price + " " + heavyNotebook.year);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price+ " " + heavyNotebook.year);
         heavyNotebook.checkPrice();
         heavyNotebook.checkWeight();
         heavyNotebook.checkYear();
 
         Notebook oldNotebook = new Notebook(1600, 500, 2005);
-        System.out.println(oldNotebook.weight + " " + oldNotebook.price + " " + oldNotebook.year);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price+ " " + oldNotebook.year);
         oldNotebook.checkPrice();
         oldNotebook.checkWeight();
         oldNotebook.checkYear();
