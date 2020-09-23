875b9a1a0f8565983ee95bd25d76d3f753ce8ad7
Mateusz Mekarski <matmeka6@gmail.com>
2020-09-23 19:08:54 +0200

    2.5

--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -1,5 +1,3 @@
-package com.kodilla.advance;
-
 public class User {
     String name;
     int age;
@@ -12,8 +10,8 @@ public class User {
     public static void main(String[] args) {
         User[] users = {
                 new User("Tom",18),
-                new User("Chris", 30),
-                new User("Luke", 40),
+                new User("Chris", 60),
+                new User("Luke", 70),
                 new User("Kate", 25)};
         for (int i = 0; i<users.length; i++);
         double age = 0;
