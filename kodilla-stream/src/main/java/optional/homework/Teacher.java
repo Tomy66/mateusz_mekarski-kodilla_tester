9934c664a672a43cf50fcd037986c5c5edcfbcdb
Mateusz Mekarski <matmeka6@gmail.com>
2020-09-30 19:16:44 +0200

    6.2 do przerobienia

---
+++ kodilla-stream/src/main/java/optional/homework/Teacher.java
@@ -0,0 +1,13 @@
+package optional.homework;
+
+public class Teacher {
+    String name;
+
+    public Teacher(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
