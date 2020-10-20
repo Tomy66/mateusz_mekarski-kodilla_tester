5540088d8103668c559145c01fa9697dd3822f01
Mateusz Mekarski <matmeka6@gmail.com>
2020-10-20 17:02:29 +0200

    13.4

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -1,23 +1,26 @@
 package com.kodilla.selenium.allegro;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class AllegroTestingApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.setProperty("webdriver.chrome.driver","C:\\selenium drivers\\chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://allegro.pl/");
 
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
+        TimeUnit.MILLISECONDS.sleep(100);
+        driver.switchTo().activeElement().sendKeys(Keys.ENTER);
 
-        WebElement inputField = driver.findElement(By.xpath("//*[@id=\"main-wrapper\"]/div/header/div/div/div/div/form/input/"));
-        inputField.sendKeys("Mavic mini");
-        inputField.submit();
+        WebElement category = driver.findElement(By.xpath("/html/body/div[2]/div[5]/div/div/div[1]/div/div/div/div/div/div[2]/div/nav/div/div/div[2]/div/div/div[1]/a"));
+        category.click();
+
+
+        WebElement input = driver.findElement(By.cssSelector("input[type='search']"));
+        input.sendKeys("Mavic mini");
+        input.submit();
 
     }
 }
