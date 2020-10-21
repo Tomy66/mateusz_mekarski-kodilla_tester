d4f228a2fcae2183956bad35090c1591646a4f48
Mateusz Mekarski <matmeka6@gmail.com>
2020-10-21 20:26:39 +0200

    13.5

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -18,15 +18,14 @@ public class AllegroTestingApp {
 
         WebElement category = driver.findElement(By.cssSelector("body > div.main-wrapper > div:nth-child(5) > div > div > div:nth-child(1) > div > div > div > div > div > div:nth-child(2) > div > nav > div > div > div.mg9e_16.mvrt_0.mh36_0.mj7a_0._9plbd._4b3d3_3La74._152v3 > div > div > div:nth-child(1)"));
         category.click();
+        TimeUnit.MILLISECONDS.sleep(100);
 
         WebElement input = driver.findElement(By.cssSelector("input[type='search']"));
         input.sendKeys("Mavic mini");
         input.submit();
+        TimeUnit.MILLISECONDS.sleep(100);
 
-        //WebDriverWait wait = new WebDriverWait(driver, 5);
-        //wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("input[type='search']")));
-
-        List<WebElement> elements = driver.findElements(By.id("#Oferty promowane"));
+        List<WebElement> elements = driver.findElements(By.tagName("article"));
         elements.get(1).getText();
 
     }
